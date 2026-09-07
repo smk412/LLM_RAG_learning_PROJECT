7a4e017f3f8a4142c52c3da377877b4849fcdecf
smk412 <whu147770@gmail.com>
2026-09-07 15:11:08 +0900

    chore: initialize Spring Boot RAG project

---
+++ src/test/java/io/github/smk412/llmraglab/RagPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.smk412.llmraglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
